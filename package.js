2cb7c326f9fa30a59829003dc888610ed600f7aa
Dmitriy Haponov <d.haponov@111minutes.com>
2015-10-11 16:14:08 +0300

    fix deps

--- package.js
+++ package.js
@@ -1,12 +1,17 @@
 Package.describe({
   name: 'sunstory:meteor-template-helpers',
   summary: 'Useful template helpers.',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/sunstorymvp/meteor-template-helpers.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
-  api.use(['coffeescript', 'underscore']);
+  api.use([
+    'coffeescript',
+    'underscore',
+    'check',
+    'session'
+  ]);
   api.addFiles('meteor-template-helpers.coffee', 'client');
 });
